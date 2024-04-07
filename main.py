4eafc4476857c864e32f36200ab724c3cd24ebcb
Osazeme Usen <osazeme.usen@casper.com>
2024-04-07 02:39:22 -0400

    setup similarity search using chroma for vectordb

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from langchain.document_loaders import TextLoader
+from langchain.embeddings import OpenAIEmbeddings
 from langchain.text_splitter import CharacterTextSplitter
+from langchain.vectorstores.chroma import Chroma
 from dotenv import load_dotenv
 
 load_dotenv()
+
+embeddings = OpenAIEmbeddings()
+
+# emb = embeddings.embed_query("hi there")
+# print(emb)
 
 text_splitter = CharacterTextSplitter(
     separator="\n",
@@ -13,5 +20,18 @@ text_splitter = CharacterTextSplitter(
 loader = TextLoader("facts.txt")
 docs = loader.load_and_split(text_splitter=text_splitter)
 
-for doc in docs:
-    print(doc.page_content)
+db = Chroma.from_documents(
+    docs,
+    embedding=embeddings,
+    persist_directory="emb"
+)
+
+results = db.similarity_search(
+    "What is an interesting fact about the english language?",
+    k=1
+)
+
+for result in results:
+
+    print("\n")
+    print(result.page_content)
